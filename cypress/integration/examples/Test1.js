091b2062320d8e57a11a7e673b70420b8d15fca1
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2022-05-18 14:04:34 -0500

    Init proyect

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,9 @@
+describe('My first test suite', function() {
+    it('My first test case', function() {
+        cy.visit("https://www.rahulshettyacademy.com/seleniumPractise/#/")
+    })
+
+    it('My second test case', function() {
+        
+    })
+})
